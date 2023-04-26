8015e6c292bf32b3f773fe021aadbeace652d0f2
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-04-26 20:24:14 +0200

    Add files via upload

---
+++ src/locations/fetchLocations.js
@@ -0,0 +1,39 @@
+async function getWildLocations(locations){
+    footerP("Fetching wild locations")
+    const rawWildLocations = await fetch(`https://raw.githubusercontent.com/${repo}/src/data/wild_encounters.json`)
+    const jsonWildLocations = await rawWildLocations.json()
+
+    return regexWildLocations(jsonWildLocations, locations)   
+}
+
+async function buildLocationsObj(){
+    let locations = {}
+
+    locations = await getWildLocations(locations)
+
+    await localStorage.setItem("locations", LZString.compressToUTF16(JSON.stringify(locations)))
+    return locations
+}
+
+
+async function fetchLocationsObj(){
+    if(!localStorage.getItem("locations")){
+        window.locations = await buildLocationsObj()
+    }
+    else{
+        window.locations = await JSON.parse(LZString.decompressFromUTF16(localStorage.getItem("locations")))   
+    }
+
+    let counter = 0
+    window.locationsTracker = []
+    Object.keys(locations).forEach(zone => {
+        Object.keys(locations[zone]).forEach(method => {
+            Object.keys(locations[zone][method]).forEach(speciesName => {
+                locationsTracker[counter] = {}
+                locationsTracker[counter]["key"] = `${zone}\\${method}\\${speciesName}`
+                locationsTracker[counter]["filter"] = []
+                counter++
+            })
+        })
+    })
+}
